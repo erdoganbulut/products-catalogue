92329e5f6a3d7e91fc08bc86791c83111e360f04
Erdoğan Bulut <e-bulut@hotmail.com.tr>
2022-08-25 00:53:37 +0300

    Create appheader and appfooter

---
+++ src/components/shareds/AppFooter/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const AppFooter = () => <div className="component is-app-footer">App Footer Works!</div>;
+
+export default AppFooter;
---
+++ src/components/shareds/AppHeader/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const AppHeader = () => <div className="component is-app-header">App Header Works!</div>;
+
+export default AppHeader;
